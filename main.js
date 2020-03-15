194853d0ee96c36fe671bddd96b48c6c0d863cb7
Fulvio <fulviocanducci@hotmail.com>
2020-03-14 21:38:58 -0300

    Second Commit

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-class Teste {}
+class List {
+  constructor() {
+    this.data = [];
+  }
+
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
+  }
+}
+class TodoList extends List {
+  constructor() {
+    super();
+  }
+}
+
+const minhaLista = new TodoList();
+
+var btnAdicionar = document.getElementById('btnAdicionar');
+btnAdicionar.onclick = function() {
+  minhaLista.add('New Todos');
+};
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+const newArray = array.map(x => x * 2);
